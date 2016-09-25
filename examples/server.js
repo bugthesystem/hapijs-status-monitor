9c1f864969735cc0101158692e3a1e5d564b19eb
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2016-09-25 20:11:14 +0200

    Improve example project

--- examples/server.js
+++ examples/server.js
@@ -5,29 +5,37 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
-    host: 'localhost',
-    port: 8000
+  host: 'localhost',
+  port: 8000
 });
 
 server.register({
-    register: require('hapijs-status-monitor')
+  register: require('hapijs-status-monitor')
 });
 
 
-// Add the route
+// Add the "/hello" route
 server.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function (request, reply) {
-        return reply('hello world');
-    }
+  method: 'GET',
+  path: '/hello',
+  handler: function (request, reply) {
+    return reply('hello world');
+  }
+});
+
+// Add the "/return-status/{statusCode}" route
+server.route({
+  method: 'GET',
+  path: '/return-status/{statusCode}',
+  handler: function (request, reply) {
+    const statusCode = request.params.statusCode;
+
+    return reply(statusCode).code(parseInt(statusCode));
+  }
 });
 
 // Start the server
 server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+  if (err) throw err;
+  console.log('Server running at:', server.info.uri);
 });
