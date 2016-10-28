0d9f569d35d7326b23c85143d06215a57dd43a3a
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2016-10-28 15:30:57 +0200

    Add default-config.spec.js

---
+++ __tests__/helpers/default-config.spec.js
@@ -0,0 +1,25 @@
+const defaultConfig = require('../../src/helpers/default-config');
+
+describe('helpers', () => {
+  describe('default-config', () => {
+    describe('when default config', () => {
+      it(`then title === hapi.js Status`, () => {
+        expect('hapi.js Status').toEqual(defaultConfig.title);
+      });
+
+      it(`then path === /status`, () => {
+        expect('/status').toEqual(defaultConfig.path);
+      });
+
+      it(`then spans === array`, () => {
+        const spans = [
+          { interval: 1, retention: 60 },
+          { interval: 5, retention: 60 },
+          { interval: 15, retention: 60 },
+        ];
+
+        expect(spans).toEqual(defaultConfig.spans);
+      });
+    });
+  });
+});
