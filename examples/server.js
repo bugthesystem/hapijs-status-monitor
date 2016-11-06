a94afe666593d048284d08220c4cabaa5203bd95
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2016-11-06 10:43:02 +0100

    Lint examples/server.js

--- examples/server.js
+++ examples/server.js
@@ -1,31 +1,28 @@
-'use strict';
-
 const Hapi = require('hapi');
+const hapijsStatusMonitor = require('hapijs-status-monitor');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
   host: 'localhost',
-  port: 8000
+  port: 8000,
 });
 
-server.register({
-  register: require('hapijs-status-monitor')
-});
+server.register({ register: hapijsStatusMonitor });
 
 // Add the "/return-status/{statusCode}" route
 server.route({
   method: 'GET',
   path: '/return-status/{statusCode}',
-  handler: function (request, reply) {
+  handler: function handler(request, reply) {
     const statusCode = request.params.statusCode;
 
-    return reply(statusCode).code(parseInt(statusCode));
-  }
+    return reply(statusCode).code(parseInt(statusCode, 10));
+  },
 });
 
 // Start the server
 server.start((err) => {
   if (err) throw err;
-  console.log('Server running at:', server.info.uri);
+  console.log('Server running at:', server.info.uri); // eslint-disable-line no-console
 });
