906cc3c3dd6d4443055530bd93f0cf130d80f95f
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2016-11-06 11:03:22 +0100

    Lint __tests__/helpers/default-config.spec.js

--- __tests__/helpers/default-config.spec.js
+++ __tests__/helpers/default-config.spec.js
@@ -3,15 +3,15 @@ const defaultConfig = require('../../src/helpers/default-config');
 describe('helpers', () => {
   describe('default-config', () => {
     describe('when default config', () => {
-      it(`then title === hapi.js Status`, () => {
+      it('then title === hapi.js Status', () => {
         expect('hapi.js Status').toEqual(defaultConfig.title);
       });
 
-      it(`then path === /status`, () => {
+      it('then path === /status', () => {
         expect('/status').toEqual(defaultConfig.path);
       });
 
-      it(`then spans === array`, () => {
+      it('then spans === array', () => {
         const spans = [
           { interval: 1, retention: 60 },
           { interval: 5, retention: 60 },
