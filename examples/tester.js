d8136a14be3394a7350e99f578ff488f8cac6022
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2016-11-06 10:39:12 +0100

    Lint examples/tester.js

--- examples/tester.js
+++ examples/tester.js
@@ -4,7 +4,7 @@ const requestUrl = 'http://localhost:8000/return-status/';
 const interval = 50;
 
 const makeDummyCall = () => setTimeout(() => {
-  const code = parseInt(200 + Math.random() * 399);
+  const code = parseInt(200 + (Math.random() * 399), 10);
   request.get(`${requestUrl}${code}`);
   makeDummyCall();
 }, interval);
