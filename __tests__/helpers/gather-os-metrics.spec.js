cfd18eb10199e53b56d98b89cab4253eda8a7cec
Wessel dR <j.w.deroode@gmail.com>
2018-03-31 08:55:24 +0200

    FIX #27, pidusage.stat not a function (#28)

--- __tests__/helpers/gather-os-metrics.spec.js
+++ __tests__/helpers/gather-os-metrics.spec.js
@@ -1,16 +1,14 @@
 const gatherOsMetrics = require('../../src/helpers/gather-os-metrics');
+
+jest.mock('pidusage');
 const pidusage = require('pidusage');
 
 describe('helpers', () => {
   describe('gather-os-metrics', () => {
     describe('when invoked', () => {
-      it('then pidusage.stat should be called', () => {
-        const span = { os: [], responses: [] };
-        pidusage.stat = jest.fn();
-
-        gatherOsMetrics({}, span);
-
-        expect(pidusage.stat).toHaveBeenCalled();
+      it('then pidusage should be called', () => {
+        gatherOsMetrics({}, { os: [], responses: [] });
+        expect(pidusage).toHaveBeenCalled();
       });
     });
   });
