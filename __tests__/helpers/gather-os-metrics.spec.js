95d0debe4506c56f00e05cb276e35a127cef6c57
Wessel dR <j.w.deroode@gmail.com>
2018-03-31 02:12:13 +0200

    Update gather-os-metrics.spec.js
    Let's hope this works :-/

--- __tests__/helpers/gather-os-metrics.spec.js
+++ __tests__/helpers/gather-os-metrics.spec.js
@@ -1,15 +1,13 @@
 const gatherOsMetrics = require('../../src/helpers/gather-os-metrics');
+
+jest.mock('pidusage');
 const pidusage = require('pidusage');
 
 describe('helpers', () => {
   describe('gather-os-metrics', () => {
     describe('when invoked', () => {
       it('then pidusage should be called', () => {
-        const span = { os: [], responses: [] };
-        pidusage = jest.fn();
-
-        gatherOsMetrics({}, span);
-
+        gatherOsMetrics({}, { os: [], responses: [] });
         expect(pidusage).toHaveBeenCalled();
       });
     });
